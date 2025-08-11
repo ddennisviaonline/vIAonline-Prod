7a174699cc84194f5c162c9455732fd5ce290828
ddennisviaonline <ddennis@viaonline.com.ar>
2025-08-11 15:12:28 -0300

    Add files via upload

--- MakeFileTXT/run.ps1
+++ MakeFileTXT/run.ps1
@@ -1,51 +1,32 @@
 ﻿param($Request, $TriggerMetadata)
 
-# Contenido para el archivo TXT
-$fileContent = "Este es un archivo generado desde Azure Function el $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-
-# Parámetros del repo GitHub
+# ==== CONFIGURACIÓN GITHUB ====
 $owner = "ddennisviaonline"
 $repo = "vIAonline-Prod"
-$path = "archivo.txt"  # Ejemplo: "archivos/archivo.txt"
-$branch = "master"
-
-# Token almacenado en Application Settings
+$adsPath = "lista.csv"  # csv de publicidades
+$vIAcache = "vIAcache.csv" # csv de historico 100 ultimos links
+$fileindexhtml = "index.html"
+$filevIAcache = "vIAcache.csv"
+$txtPath = "archivo.txt"
+$branchsource = "main" # powershell
+$branch = "master" # webpage
 $token = $env:GitHubToken
 
-# Obtener SHA del archivo para actualizarlo (si existe)
-$uriGet = "https://api.github.com/repos/$owner/$repo/contents/$path?ref=$branch"
+# ==== 1. DESCARGAR ads.CSV DESDE GITHUB ====
+$csvUri = "https://api.github.com/repos/$owner/$repo/contents/$path?ref=$branchsource"
+
 try {
-    $response = Invoke-RestMethod -Uri $uriGet -Headers @{ Authorization = "token $token"; "User-Agent" = "PowerShell" } -Method GET
+    $response = Invoke-RestMethod -Uri $csvUri -Headers @{ Authorization = "token $token"; "User-Agent" = "PowerShell" } -Method GET
     $sha = $response.sha
 } catch {
     # El archivo no existe, no se necesita sha
     $sha = $null
 }
 
-# Codificar contenido en Base64
-$contentBase64 = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($fileContent))
+$logo = "https://github.com/ddennisviaonline/vIAonline-Prod/blob/master/Imagenes/Logocorto.png?raw=true" # CREAR DNS PARA QUE APUNTE A ESTE LINK
 
-# Crear cuerpo para el PUT (crear o actualizar)
-$body = @{
-    message = "Creación o actualización archivo desde Azure Function"
-    content = $contentBase64
-    branch = $branch
-}
-if ($sha) { $body.sha = $sha }
-
-$jsonBody = $body | ConvertTo-Json -Depth 10
-
-# Subir archivo al repo
-$uriPut = "https://api.github.com/repos/$owner/$repo/contents/$path"
-$responsePut = Invoke-RestMethod -Uri $uriPut -Headers @{ Authorization = "token $token"; "User-Agent" = "PowerShell" } -Method PUT -Body $jsonBody
-
-# Respuesta
-$bodyOut = @{
-    message = "Archivo TXT guardado en GitHub correctamente"
-    commitUrl = $responsePut.commit.html_url
-} | ConvertTo-Json
-
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = 200
-    Body = $bodyOut
-})
+# 1. Carga el HTML desde archivo
+#$html = Get-Content -Path ".\archivo.html" -Raw
+$URLOrigen = "https://infobae.com"
+$response = Invoke-WebRequest -Uri $URLOrigen
+$html = $response.Content
