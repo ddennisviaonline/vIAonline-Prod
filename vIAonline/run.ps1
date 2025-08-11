497b6ad188714d6f5d982d782c5441bfa8aedd95
ddennisviaonline <ddennis@viaonline.com.ar>
2025-08-11 15:18:27 -0300

    Add files via upload

--- vIAonline/run.ps1
+++ vIAonline/run.ps1
@@ -30,3 +30,38 @@ $logo = "https://github.com/ddennisviaonline/vIAonline-Prod/blob/master/Imagenes
 $URLOrigen = "https://infobae.com"
 $response = Invoke-WebRequest -Uri $URLOrigen
 $html = $response.Content
+
+# 1. Capturamos cada bloque <a ...>...</a> que contiene la noticia
+$patternBloques = '<a\s+href="(?<url>/[^"]+)"[^>]*class="story-card-ctn">.*?<\/a>'
+
+$bloques = [regex]::Matches($html, $patternBloques, [System.Text.RegularExpressions.RegexOptions]::Singleline)
+
+$Articulos = foreach ($bloque in $bloques) {
+    $htmlBloque = $bloque.Value
+
+    # Extraemos el título
+    $titulo = ([regex]::Match($htmlBloque, '<h2[^>]*>(?<titulo>.*?)<\/h2>', [System.Text.RegularExpressions.RegexOptions]::Singleline)).Groups['titulo'].Value.Trim()
+    $titulo = $titulo -replace '\s+', ' '
+
+    # Extraemos el autor
+    $autor = ([regex]::Match($htmlBloque, '<b[^>]*class="story-card-author-name"[^>]*>(?<autor>.*?)<\/b>', [System.Text.RegularExpressions.RegexOptions]::Singleline)).Groups['autor'].Value.Trim()
+
+    # URL
+    $url = $bloque.Groups['url'].Value.Trim()
+
+    [PSCustomObject]@{
+        URL    = $URLOrigen + $url
+        Titulo = $titulo
+        Autor  = $autor
+    }
+}
+
+# $Articulos # | Export-Csv -Path "resultados.csv" -NoTypeInformation -Encoding UTF8
+
+#
+# Una vez que tenemos los titulos y los url tomamos el url y consultamos a la IA TITULO, INTRO, NOTA y DATOS
+#
+
+#función crear titulos por IA
+
+#FUNCIONES
