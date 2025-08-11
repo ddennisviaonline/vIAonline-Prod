4cb36136972f4be4e6c70191316ae51844245499
ddennisviaonline <ddennis@viaonline.com.ar>
2025-08-11 15:21:00 -0300

    Add files via upload

--- vIAonline/run.ps1
+++ vIAonline/run.ps1
@@ -65,3 +65,33 @@ $Articulos = foreach ($bloque in $bloques) {
 #función crear titulos por IA
 
 #FUNCIONES
+
+function consulta-IA {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet("Titulo", "Intro", "Nota", "Datos")]
+        [string]$tipo,
+
+        [Parameter(Mandatory)]
+        [string]$linkFuente
+    )
+    switch ($tipo) {
+        "Titulo" {
+            return "Se consultó el Titulo a la IA"
+        }
+        "Intro" {
+            return "Se consultó la Intro a la IA"
+        }
+        "Nota" {
+            return "Se consultó la Nota a la IA"
+        }
+        "Datos" {
+            return "Se consultó el Datos a la IA"
+        }
+        default {
+            Write-Warning "Acción no reconocida."
+        }
+    }
+    
+}
